03bd3273d05d72152b4b9213efdd9c3fc096f880
choshing <choshing.cheung@go.shoreline.edu>
2024-02-04 00:44:39 -0800

    Adding Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar.js"
+import Home from "./Components/Home.js"
+import About from "./Components/About.js"
+import Skill from "./Components/Skill.js"
+import Project from "./Components/Project.js"
+import Contact from "./Components/Contact.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <Home />
+      <About />
+      <Skill />
+      <Project />
+      <Contact />
+    </main>
   );
 }
 
