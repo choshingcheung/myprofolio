b06129982ee78b07959680ce3e944b2a717f747c
choshing <choshing.cheung@go.shoreline.edu>
2024-02-09 02:18:29 -0800

    Reseting everything to normal and importing tailwind. Ready to start modifying home component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import Navbar from "./Components/Navbar.js"
+import React from 'react'
 import Home from "./Components/Home.js"
-import About from "./Components/About.js"
-import Skill from "./Components/Skill.js"
-import Project from "./Components/Project.js"
-import Contact from "./Components/Contact.js"
+
 
 function App() {
   return (
     <main>
-      <Navbar />
       <Home />
-      <About />
-      <Skill />
-      <Project />
-      <Contact />
     </main>
   );
 }
